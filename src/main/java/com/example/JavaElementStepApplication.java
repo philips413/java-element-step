5d4fde1746c4df71967c260f18d572c7d5d8c753
Philips413 <philips413@kakao.com>
2023-05-10 07:21:38 +0900

    create: init setting

---
+++ src/main/java/com/example/JavaElementStepApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaElementStepApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaElementStepApplication.class, args);
+    }
+
+}
